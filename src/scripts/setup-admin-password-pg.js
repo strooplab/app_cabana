bc30d4f6724c4cece20d2986750dc51569c73369
strooplab <strooplab@outlook.com>
2025-10-20 12:44:26 -0500

    Fixed script setup-admin-password

--- src/scripts/setup-admin-password-pg.js
+++ src/scripts/setup-admin-password-pg.js
@@ -1,6 +1,6 @@
 // scripts/setup-admin-password-pg.js
 import bcrypt from 'bcryptjs';
-import pool from '../lib/db';
+import pool from '../lib/db.ts';
 import readline from 'readline';
 
 async function ask(question) {
